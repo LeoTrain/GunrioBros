3b6657ef5a11ec3fd8f3cd331b11683754d5bf02
FT_Leo <leonardo.bertonasco01@gmail.com>
2025-03-06 11:45:47 +0100

    Better object movement
    Made a better object movement that removes the sliding, I've used acceleration and deceleration and add the Force to the rigidbody.

--- Game/Assets/BoxScript.cs
+++ Game/Assets/BoxScript.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class BoxScript : MonoBehaviour
 {
     public Rigidbody2D myRigidBody;
-    public float speed = 4;
+    public float speed = 4f;
+    public float acceleration = 15f;
+    public float deceleration = 10f;
     public float jumpHeight = 7;
     public int jumpCount = 0;
+
+    private float moveInput = 0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,12 +22,23 @@ public class BoxScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.D) && this.jumpCount == 0)
-            myRigidBody.linearVelocity = Vector2.right * this.speed;
-        if (Input.GetKey(KeyCode.A) && this.jumpCount == 0)
-            myRigidBody.linearVelocity = Vector2.left * this.speed;
-        if (Input.GetKeyDown(KeyCode.Space) && this.jumpCount == 0)
-            this.Jump();
+        this.moveInput = 0f;
+        if (Input.GetKey(KeyCode.D)) this.moveInput = 1f;
+        if (Input.GetKey(KeyCode.A)) this.moveInput = -1f;
+        if (Input.GetKeyDown(KeyCode.Space) && this.jumpCount == 0) this.Jump();
+    }
+
+    void FixedUpdate()
+    {
+        this.Move();
+    }
+
+    void Move()
+    { 
+        float targetVelocity = this.moveInput * this.speed;
+        float velocityDifference = targetVelocity - this.myRigidBody.linearVelocity.x;
+        float force = (this.moveInput != 0) ? this.acceleration * velocityDifference : -this.deceleration * this.myRigidBody.linearVelocity.x;
+        this.myRigidBody.AddForce(Vector2.right * force, ForceMode2D.Force);
     }
 
     void Jump()
