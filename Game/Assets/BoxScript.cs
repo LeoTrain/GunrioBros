f964d81884b30d39d36d66e28b174416fc90b6db
FT_Leo <leonardo.bertonasco01@gmail.com>
2025-03-06 10:48:20 +0100

    Added jump count
    This allows the object to only jump once and resets the jumpCount as soon as he collides with all objects tagged as Terrain.

--- Game/Assets/BoxScript.cs
+++ Game/Assets/BoxScript.cs
@@ -6,6 +6,8 @@ public class BoxScript : MonoBehaviour
 {
     public Rigidbody2D myRigidBody;
     public float speed = 4;
+    public float jumpHeight = 7;
+    public int jumpCount = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,7 +22,18 @@ public class BoxScript : MonoBehaviour
             myRigidBody.linearVelocity = Vector2.right * this.speed;
         if (Input.GetKey(KeyCode.A))
             myRigidBody.linearVelocity = Vector2.left * this.speed;
-        if (Input.GetKeyDown(KeyCode.Space))
-            myRigidBody.linearVelocity = Vector2.up * 5;
+        if (Input.GetKeyDown(KeyCode.Space) && this.jumpCount == 0)
+        { 
+            myRigidBody.linearVelocity = Vector2.up * this.jumpHeight;
+            this.jumpCount = 1;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Terrain")
+        {
+            this.jumpCount = 0;
+        }
     }
 }
