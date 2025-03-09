83d2741016e0292f3bc27568cdb512eaf6d1673d
evargaa <vendere10@gmail.com>
2025-03-09 11:47:54 +0100

    added PlayerHealth

--- Game/Assets/Enemy.cs
+++ Game/Assets/Enemy.cs
@@ -33,12 +33,14 @@ public class Enemy : MonoBehaviour
   void OnCollisionEnter2D(Collision2D collision)
   {
       if (collision.gameObject.tag == "Player" && this.ComesFromAbove(collision.gameObject.transform.position.y))
+      {
         Destroy(this.gameObject);
+      }
   }
 
   bool IsPlayerinRadius()
   {
-    
+    return false;
   }
 
   Vector2 CalculateBoundaries()
