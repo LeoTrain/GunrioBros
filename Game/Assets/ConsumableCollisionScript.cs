1c48f60637ae0b2fc979d16fce42f950777f056d
FT_Leo <ftleo@Leonardos-MacBook-Air.local>
2025-03-11 11:15:17 +0100

    added growth duration

--- Game/Assets/ConsumableCollisionScript.cs
+++ Game/Assets/ConsumableCollisionScript.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 public class ConsumableCollisionScript : MonoBehaviour
 {
+    private bool isBigger = false;
+    private DateTime lastGrowTime;
+    private float growDuration = 5.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +15,11 @@ public class ConsumableCollisionScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+      if (isBigger && DateTime.Now.Subtract(lastGrowTime).TotalSeconds > growDuration)
+      {
+        this.transform.localScale = new Vector3(2, 2, 1);
+        isBigger = false;
+      }
         
     }
 
@@ -19,5 +27,7 @@ public class ConsumableCollisionScript : MonoBehaviour
     {
       // To modify with the actual growing logic
       this.transform.localScale = new Vector3(4, 4, 1);
+      isBigger = true;
+      lastGrowTime = DateTime.Now;
     }
 }
